1a67935e9a47618aa045d1ce4db334eed9bbc99b
charuzain <charuabhizain@gmail.com>
2025-05-27 14:47:19 -0400

    Created redux slice to manage light/dark mode

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import countryReducer from '../slice/countrySlice';
+import themeReducer from '../slice/themeSlice';
 
 export const store = configureStore({
   reducer: {
     country: countryReducer,
+    theme: themeReducer,
   },
 });
 
