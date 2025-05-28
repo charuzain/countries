182bb11d6b7ceea2eebbb194622b06140562354e
charuzain <charuabhizain@gmail.com>
2025-05-28 15:42:18 -0400

    Add region filter dropdown with toggle and selection functionality

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from 'react';
+import { MdOutlineKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
 
 const Filter = () => {
-  return (
-    <div>Filter</div>
-  )
-}
+  const [selectFilter, setSelectFilter] = useState<boolean>(false);
 
-export default Filter
+  const [filter, setfilter] = useState<string>('');
+
+  const filterClickHandler = () => {
+    setSelectFilter(!selectFilter);
+  };
+
+  const filterSelectHandler = (value: string) => {
+    console.log(value);
+    setfilter(value);
+    setSelectFilter(!selectFilter);
+  };
+  return (
+    <>
+      <button onClick={filterClickHandler}>
+        <span>{!filter ? 'Filter By Region' : filter}</span>
+        <i>
+          {selectFilter ? (
+            <MdOutlineKeyboardArrowDown />
+          ) : (
+            <MdKeyboardArrowUp />
+          )}
+        </i>
+      </button>
+      {selectFilter && (
+        <ul>
+          <li onClick={() => filterSelectHandler('Africa')}>Africa</li>
+          <li onClick={() => filterSelectHandler('Americas')}>Americas</li>
+          <li onClick={() => filterSelectHandler('Asia')}>Asia</li>
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Filter;
