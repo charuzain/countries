e70c4f931ea902cb7f86d6cbfa83491ad270715b
charuzain <charuabhizain@gmail.com>
2025-05-29 01:37:36 -0400

    Minor css changes to countrycard

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -1,4 +1,5 @@
 import styles from './CoountryCard.module.css';
+import { Link } from 'react-router';
 
 const CountryCard = ({
   name,
@@ -15,24 +16,26 @@ const CountryCard = ({
 }) => {
   return (
     <li className={styles['country-card']}>
-      <div className={styles['image-box']}>
-        <img src={flag} alt={`${name} flag`} className={styles['image']} />
-      </div>
-      <p className={styles['country-name']}>{name}</p>
-      <div className={styles['country-details']}>
-        <p className={styles['country-detail-label']}>
-          Population:
-          <span className={styles['country-info']}>   {population}</span>
-        </p>
-        <p className={styles['country-detail-label']}>
-          Region:
-          <span className={styles['country-info']}> {region}</span>
-        </p>
-        <p className={styles['country-detail-label']}>
-          Capital:
-          <span className={styles['country-info']}> {capital}</span>
-        </p>
-      </div>
+      <Link to={`/${name}`} className={styles['link']}>
+        <div className={styles['image-box']}>
+          <img src={flag} alt={`${name} flag`} className={styles['image']} />
+        </div>
+        <p className={styles['country-name']}>{name}</p>
+        <div className={styles['country-details']}>
+          <p className={styles['country-detail-label']}>
+            Population:
+            <span className={styles['country-info']}> {population}</span>
+          </p>
+          <p className={styles['country-detail-label']}>
+            Region:
+            <span className={styles['country-info']}> {region}</span>
+          </p>
+          <p className={styles['country-detail-label']}>
+            Capital:
+            <span className={styles['country-info']}> {capital}</span>
+          </p>
+        </div>
+      </Link>
     </li>
   );
 };
