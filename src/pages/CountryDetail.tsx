cce9ba758651117a928f497d3ccc50d1471193a4
charuzain <charuabhizain@gmail.com>
2025-05-29 01:36:32 -0400

    dispatch fetchCountryByName thunk in CountryDetail using useParams

--- src/pages/CountryDetail.tsx
+++ src/pages/CountryDetail.tsx
@@ -1,8 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from '../app/store';
+import { useEffect } from 'react';
+import { fetchCountryByName } from '../slice/countrySlice';
+import { useParams } from 'react-router';
 
 const CountryDetail = () => {
-  return (
-    <div>CountryDetail</div>
-  )
-}
+  const { name } = useParams<{ name: string }>();
+  const dispatch = useDispatch<AppDispatch>();
+  const selectedCountry = useSelector(
+    (state: RootState) => state.country.selectedCountry
+  );
+  
+  console.log(selectedCountry);
 
-export default CountryDetail
+  useEffect(() => {
+    if (name) {
+      dispatch(fetchCountryByName(name));
+    }
+  }, [dispatch, name]);
+
+  return <div>CountryDetail</div>;
+};
+
+export default CountryDetail;
